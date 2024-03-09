35d0b9f19fde61eeedfde7d5bb9d8009d2f91b8b
Soham Ghugare <soham.ghugare@gmail.com>
2024-03-09 15:26:09 +0530

    feat(model): added create case model

--- server/src/models/case.rs
+++ server/src/models/case.rs
@@ -17,3 +17,26 @@ pub struct Location {
     pub latitude: f32,
     pub longitude: f32,
 }
+
+// create case model
+#[derive(Serialize, Deserialize)]
+pub struct CreateCase {
+    pub camera_id: String,
+    pub footage_url: String,
+    pub camera_location: Location,
+}
+
+// methods for case
+impl Case {
+    pub fn new(case: CreateCase) -> Self {
+        // generate random id
+        let id = rand::random::<u16>();
+        Case {
+            id,
+            camera_id: case.camera_id,
+            footage_url: case.footage_url,
+            camera_location: case.camera_location,
+            timestamp: Utc::now(),
+        }
+    }
+}
