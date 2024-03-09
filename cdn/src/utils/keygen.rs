e822fa723ddb3631bb30c4d7fe461ba0d1c1be63
Soham Ghugare <soham.ghugare@gmail.com>
2024-03-09 11:40:11 +0530

    feat(util): added keygen function

---
+++ cdn/src/utils/keygen.rs
@@ -0,0 +1,15 @@
+use rand::Rng;
+
+pub fn generate_key(length: u8) -> String {
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let mut rng = rand::thread_rng();
+
+    let key: String = (0..length)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    key
+}
