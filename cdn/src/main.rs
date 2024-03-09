e8e2b245d9456b3f5bbcbc351f0348bebba33a5a
Soham Ghugare <soham.ghugare@gmail.com>
2024-03-09 11:37:14 +0530

    feat(logger): set up tracing

--- cdn/src/main.rs
+++ cdn/src/main.rs
@@ -1,8 +1,16 @@
 #[macro_use]
 extern crate rocket;
 
+// modules
+mod handlers;
+mod models;
+mod responses;
+
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
+    // setting up logger
+    tracing_subscriber::fmt::init();
+
     let rocket = rocket::build();
 
     rocket.launch().await?;
