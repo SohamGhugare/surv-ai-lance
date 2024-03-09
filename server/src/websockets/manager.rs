b8b36baed91f55ccff1b40ae08c55a7918bbd0c8
Soham Ghugare <soham.ghugare@gmail.com>
2024-03-09 17:27:04 +0530

    refactor: refactored connection manager

--- server/src/websockets/manager.rs
+++ server/src/websockets/manager.rs
@@ -6,36 +6,33 @@ use std::{
 use rocket::futures::SinkExt;
 
 pub struct ConnectionManager {
-    connections: Arc<Mutex<HashMap<String, ws::stream::DuplexStream>>>,
+    connections: HashMap<String, Arc<Mutex<ws::stream::DuplexStream>>>,
 }
 
 impl ConnectionManager {
     // initializing connection manager
     pub async fn init() -> Self {
         ConnectionManager {
-            connections: Arc::new(Mutex::new(HashMap::new())),
+            connections: HashMap::new(),
         }
     }
 
     // adding a new connection
-    pub async fn add_connection(&mut self, id: String, connection: ws::stream::DuplexStream) {
-        self.connections
-            .lock()
-            .unwrap()
-            .insert(id.clone(), connection);
+    pub async fn add_connection(&mut self, id: String, connection: Arc<Mutex<ws::stream::DuplexStream>>>) {
+        self.connections.insert(id.clone(), connection);
         info!("Added connection with id {:?}", id);
     }
 
     // dropping a connection
     pub async fn remove_connection(&mut self, id: &str) {
-        self.connections.lock().unwrap().remove(id);
+        self.connections.remove(id);
         info!("Dropped connection with id {:?}", id);
     }
 
     // sending a message to connection
     pub async fn send_message(&mut self, id: &str, message: ws::Message) {
-        if let Some(connection) = self.connections.lock().unwrap().get_mut(id) {
-            let _ = connection.send(message);
+        if let Some(connection) = self.connections.get_mut(id) {
+            let _ = connection.lock().unwrap().send(message);
         }
     }
 }
