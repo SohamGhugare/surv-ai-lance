31d3c4f6c31ad5b48d697a7ed7550fd999dcb462
Soham Ghugare <soham.ghugare@gmail.com>
2024-03-09 12:02:41 +0530

    feat: added dotenv loading and mounted upload route

--- cdn/src/main.rs
+++ cdn/src/main.rs
@@ -1,3 +1,5 @@
+use handlers::upload_handler::upload;
+
 #[macro_use]
 extern crate rocket;
 
@@ -5,13 +7,19 @@ extern crate rocket;
 mod handlers;
 mod models;
 mod responses;
+mod utils;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     // setting up logger
     tracing_subscriber::fmt::init();
 
-    let rocket = rocket::build();
+    // loading env file
+    dotenv::dotenv().ok();
+
+    let rocket = rocket::build()
+        // <------ ROUTES ------->
+        .mount("/", routes![upload]);
 
     rocket.launch().await?;
 
