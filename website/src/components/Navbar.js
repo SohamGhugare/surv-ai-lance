12a943482500727b63a350be8f361efce50472ee
diya-kailash <diyakailash04@gmail.com>
2024-03-09 19:02:31 +0530

    added navbar

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,27 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import "../stylesheets/prep.css"
 import "../stylesheets/Navbar.css"
 
 const Navbar = () => {
+  const [navbarContainer, setNavbar] = useState(false);
+  useEffect(() => {
+    const changeBackground = () => {
+      if (window.scrollY >= 25)
+      {
+        setNavbar(true);
+      }
+      else
+      {
+        setNavbar(false);
+      }
+    };
+    window.addEventListener('scroll',changeBackground);
+    return () => {
+      window.removeEventListener('scroll',changeBackground);
+    };
+  }, []); 
   return (
     <div className='navbarParent'>
-      <div className='navbarContainer'>
+      <div className={navbarContainer ? 'navbarContainer active' : 'navbarContainer'}>
         <h2 className='Logo'>Surv-<span className='Coloured'>AI</span>-lance</h2>
         <p className='navbarContents'>About</p>
         <p className='navbarContents'>Login</p>
       </div>
     </div>
   )
-}
-const navbar = document.querySelector('#NavBar');
-let top = navbar.offsetTop;
-function stickynavbar() {
-  if (window.scrollY >= top) {    
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');    
-  }
-}
-window.addEventListener('scroll', stickynavbar);
-
-export default Navbar
+};
+export default Navbar;
