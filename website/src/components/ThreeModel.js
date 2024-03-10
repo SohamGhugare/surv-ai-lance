fc55ed4f697ffc840ddb31a5f8da2f9df41e9d87
tubule <joshi.garvit22@gmail.com>
2024-03-10 13:36:47 +0530

    Data parsing using websockets done

--- website/src/components/ThreeModel.js
+++ website/src/components/ThreeModel.js
@@ -1,30 +1,22 @@
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
+import { React, useEffect, useLayoutEffect, Suspense, useState } from 'react'
+import { Canvas } from "@react-three/fiber"
+import { PresentationControls , useGLTF , Stage } from "@react-three/drei"
+
 import { easing } from 'maath'
 
-function Suzanne(props) {
-    const mesh = useRef()
-    const { nodes } = useGLTF('./Suzanne.glb')
-    const [dummy] = useState(() => new THREE.Object3D())
-    useFrame((state, dt) => {
-        dummy.lookAt(state.pointer.x, state.pointer.y, 1)
-        easing.dampQ(mesh.current.quaternion, dummy.quaternion, 0.15, dt)
-    })
-    return (
-        <mesh ref={mesh} geometry={nodes.Suzanne.geometry} {...props}>
-            <meshNormalMaterial />
-        </mesh>
-        )
+const ModelRen = (props) => {
+    const {scene} = new useGLTF("./earth.glb")
+    return <primitive object={scene} {...props}/>
 }
 
 export default function App() {
     return (
-        <Canvas camera={{ position: [0, 0.1, 3] }}>
-            <ambientLight />
-            <directionalLight position={[10, 10, 10]} />
-            <Suzanne />
+        <Canvas dpr={[1,2]} camera={{fov:45}} style={{height:"100%",width:"100%"}}>
+            <PresentationControls speed={1} zoom={1} polar={[-2 , Math.PI / 2]}>
+                <Stage scale={1} environment={"apartment"}>
+                    <ModelRen scale={0.2}/>
+                </Stage>
+            </PresentationControls>
         </Canvas>
         )
 }
--- website/src/routes/Actions.js
+++ website/src/routes/Actions.js
@@ -5,7 +5,7 @@ import "../stylesheets/Actions.css"
 const Actions = () => {
     let socket
 
-    const [videoURI, setVideoURI] = useState()
+    const [caseData, setCaseData] = useState({})
 
     useEffect(() => {
         socket = new WebSocket("ws://172.20.10.14:8000/ws")
@@ -13,14 +13,24 @@ const Actions = () => {
             socket.send(`${Math.floor(12345 + Math.random() * 87654)}`)
         })
         socket.onmessage = encData => {
-            let decData = JSON.parse(encData)
-            setVideoURI(decData["footage_url"])
+            console.log(encData.data)
+            let decData = JSON.parse(encData["data"])
+            setCaseData(decData)
 
         }
     })
     return (
         <div className="actionsParentContainer">
-            <video src="http://localhost:5050/videos/temp.3gp" className="actionsVideo" autoPlay={true} loop={true}/>
+            <div className="actionsVideoContainer">
+                <video src={caseData["footage_url"]} className="actionsVideo" autoPlay={true} loop={true} />
+                <div className="actionsVideoTextContainer">
+                    <p><span className="coloredText">Camera ID: </span>{caseData["camera_id"]}</p>
+                    <p><span className="coloredText">Location: </span>  {caseData["camera_location"]}</p>
+                    <p><span className="coloredText">Distance: </span> 2km</p>
+                    <p><span className="coloredText">Video URI: </span> {caseData["footage_url"]}</p>
+                    <p><span className="coloredText">Timstamp: </span> {caseData["timestamp"]}</p>
+                </div>
+            </div>
             <div className="actionsButtonsContainer">
                 <button className="actionsRespond">Respond</button>
                 <button className="actionsReject">Reject</button>
